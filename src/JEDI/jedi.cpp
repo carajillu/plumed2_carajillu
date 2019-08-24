f16a43fc2df8fce0b9172f0089a126818fe2e504
carajillu <joan.clark88@gmail.com>
2019-08-24 12:58:18 +0100

    All JEDI related files are now in src/JEDI. JEDI now prints out a message and exits if the parameters file (default jedi.params) doesn't exist. In the future, this needs to be replaced by a proper plumed error and remove the jediparameters::jediparameters() method OR set default parameters and return false if the parameter file doesn't exist

---
+++ src/JEDI/jedi.cpp
@@ -0,0 +1,108 @@
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   Copyright (c) 2011-2019 The plumed team
+   (see the PEOPLE file at the root of the distribution for a list of names)
+
+   See http://www.plumed.org for more information.
+
+   This file is part of plumed, version 2.
+
+   plumed is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   plumed is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with plumed.  If not, see <http://www.gnu.org/licenses/>.
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+#ifdef _OPENMP
+    #include <omp.h>
+#else
+    #define omp_get_num_threads() 0
+    #define omp_get_thread_num() 0
+#endif
+
+#include "colvar/Colvar.h"
+#include "colvar/ActionRegister.h"
+#include "core/PlumedMain.h"
+#include "tools/Matrix.h"
+#include "tools/PDB.h"
+
+#include <string>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <vector>
+#include <ctime>
+#include <iostream>
+#include <iomanip>
+#include <sys/time.h>
+
+// Lapack needed for l2-mininum norm solution
+#include "../tools/lapack/lapack.h"
+
+#include "jediparameters.h" //Class to read JEDI parameters
+#include "kabsch.h" // kabsch algorithm for grid update
+
+
+typedef double real;
+using namespace std;
+
+
+namespace PLMD {
+namespace colvar {
+
+//Here you can put some documentation if you want
+
+class jedi : public Colvar {
+private:  
+  bool pbc;
+  jediparameters params;
+public:
+  explicit jedi(const ActionOptions&);
+// active methods:
+  virtual void calculate();
+  static void registerKeywords(Keywords& keys);
+};
+
+PLUMED_REGISTER_ACTION(jedi,"JEDI")
+
+void jedi::registerKeywords(Keywords& keys) 
+{
+  Colvar::registerKeywords(keys);
+  keys.add("compulsory","PARAMETERS","a file listing the parameters of the JEDI estimator.");
+  keys.add("compulsory","PDB","pdb file to experiment");
+}
+
+jedi::jedi(const ActionOptions&ao):
+  PLUMED_COLVAR_INIT(ao),
+  pbc(true)
+{
+  //READ jedi.parameters here
+  string parameters_file;
+  parse("PARAMETERS",parameters_file);
+  params.readParams(parameters_file);
+  //cout << "Grid resolution is " << params.resolution << endl;
+
+  addValue(); // to be replaced by AddValueWithDerivatives()
+  setNotPeriodic();
+  checkRead();
+}
+
+
+// calculator
+void jedi::calculate() {
+
+  double Jedi=12345.0;
+  setValue(Jedi);
+  double dJedi=1.0;
+}
+
+}
+}
