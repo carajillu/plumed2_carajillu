e87d9e78ae53ed412e7726d8496e5ce3aa2fc991
carajillu <joan.clark88@gmail.com>
2019-12-22 15:11:12 +0000

    Inverted the distance matrices and derivatives. Now outer vector is grid, inner is atoms. Makes more sense when coding kernel functions.

--- src/JEDI/distances.cpp
+++ src/JEDI/distances.cpp
@@ -4,7 +4,7 @@
 Once the grid is in place, the first step is to calculate the matrices that contain the euclidean distances
 between atoms j and grid points i and their derivatives with respect to the coordinates x y and z of each atom.
 These matrices have the form of vector<vector<double>> in which the positions of the external vector represent
-atoms j and the positions of the internal vectors represent grid points i. 
+grid points i and the positions of the internal vectors represent atoms j. 
 This is expensive in terms of memory, but since these distances and their derivatives are calculated a high
 number of times during the calculation of the JEDI derivatives, storing them in memory makes the code 
 significantly more efficient. 
@@ -14,26 +14,26 @@ are calculated with respect to the atom coordinates. This order guarantees the c
 */
 void distances::compute_distance_matrix(vector<PLMD::Vector> &protein, vector<PLMD::Vector> &grid)
 {
-  vector<double> r_ji_vec(grid.size(),0); //inside vector
+  vector<double> r_ij_vec(protein.size(),0); //inside vector
   
   //ugly way of initialising the vectors
-  for (unsigned j=0; j<protein.size();j++)
+  for (unsigned i=0; i<grid.size();i++)
   {
-   r_matrix.push_back(r_ji_vec);
-   dr_matrix_dx.push_back(r_ji_vec);
-   dr_matrix_dy.push_back(r_ji_vec);
-   dr_matrix_dz.push_back(r_ji_vec);
+   r_matrix.push_back(r_ij_vec);
+   dr_matrix_dx.push_back(r_ij_vec);
+   dr_matrix_dy.push_back(r_ij_vec);
+   dr_matrix_dz.push_back(r_ij_vec);
   }
 
   #pragma omp parallel for
-  for (unsigned j=0; j<protein.size();j++)
+  for (unsigned i=0; i<grid.size();i++)
   {
-      for (unsigned i=0; i<grid.size(); i++)
+      for (unsigned j=0; j<protein.size(); j++)
       {
-          r_matrix[j][i]=sqrt(pow((protein[j][0]-grid[i][0]),2)+pow((protein[j][1]-grid[i][1]),2)+pow((protein[j][2]-grid[i][2]),2));
-          dr_matrix_dx[j][i]=(protein[j][0]-grid[i][0])/r_matrix[j][i];
-          dr_matrix_dy[j][i]=(protein[j][1]-grid[i][1])/r_matrix[j][i];
-          dr_matrix_dz[j][i]=(protein[j][2]-grid[i][2])/r_matrix[j][i];
+          r_matrix[i][j]=sqrt(pow((protein[j][0]-grid[i][0]),2)+pow((protein[j][1]-grid[i][1]),2)+pow((protein[j][2]-grid[i][2]),2));
+          dr_matrix_dx[i][j]=(protein[j][0]-grid[i][0])/r_matrix[i][j];
+          dr_matrix_dy[i][j]=(protein[j][1]-grid[i][1])/r_matrix[i][j];
+          dr_matrix_dz[i][j]=(protein[j][2]-grid[i][2])/r_matrix[i][j];
       }
   }
 }
--- src/JEDI/mindist.cpp
+++ src/JEDI/mindist.cpp
@@ -2,7 +2,11 @@
 
 /*
 This function returns the following vectors:
-1) vector<double> mindist(grid_size): contains the mindist according to Eq.5 JCTC 2015 of every grid point
+1) vector<double> mindist(grid_size): contains the mindist according to Eq.5 JCTC 2015 of every grid point.
+                                      This funtion is necessary because we need a minimum distance
+                                      parameter that ensures that JEDI is continuous and differentiable
+                                      with respect to the coordinates of all atoms and not only the 
+                                      one closest to a given grid point.
 2) vector<vector<double>> d_mindist_dx(protein_size,grid_size): for every protein atom (outside vector),
                           contains the derivatives of each grid point i's (mindist) with respect of the x
                           coordinate of each atom j.
@@ -15,21 +19,22 @@ void mindist::compute_mindist(vector<vector<double>> &r_matrix,
                               vector<vector<double>> &dr_matrix_dz,
                               double &theta)
 {
-  unsigned protein_size=r_matrix.size();
-  unsigned grid_size=r_matrix[0].size();
+  unsigned grid_size=r_matrix.size();
+  unsigned protein_size=r_matrix[0].size();
 
   vector<vector<double>> exp_r; //exponential term of Eq 5 JCTC 2015
   
+  vector<double> protein_zeros(protein_size,0);
   vector<double> grid_zeros(grid_size,0);
   
   // Fill vectors with zeros
   min_dist=grid_zeros;
   for (unsigned j=0; j<protein_size;j++)
   {
-      d_mindist_dx.push_back(grid_zeros);
-      d_mindist_dy.push_back(grid_zeros);
-      d_mindist_dz.push_back(grid_zeros);
-      exp_r.push_back(grid_zeros);
+      d_mindist_dx.push_back(protein_zeros);
+      d_mindist_dy.push_back(protein_zeros);
+      d_mindist_dz.push_back(protein_zeros);
+      exp_r.push_back(protein_zeros);
   }
   
   //Calculate mindist and its derivatives
@@ -40,16 +45,16 @@ void mindist::compute_mindist(vector<vector<double>> &r_matrix,
     double sum_exp_r=0;
     for (unsigned j=0; j<protein_size;j++)
     {
-      exp_r[j][i]= exp(theta/r_matrix[j][i]);
-      sum_exp_r+=exp_r[j][i];
+      exp_r[i][j]= exp(theta/r_matrix[i][j]);
+      sum_exp_r+=exp_r[i][j];
     }
     min_dist[i]=theta/log(sum_exp_r);
     //calculate mindist derivatives
     for (unsigned j=0; j<protein_size;j++)
     {
-     d_mindist_dx[j][i]=(pow(min_dist[i],2)*exp_r[j][i])/(sum_exp_r*pow(r_matrix[j][i],2))*dr_matrix_dx[j][i];
-     d_mindist_dy[j][i]=(pow(min_dist[i],2)*exp_r[j][i])/(sum_exp_r*pow(r_matrix[j][i],2))*dr_matrix_dy[j][i];
-     d_mindist_dz[j][i]=(pow(min_dist[i],2)*exp_r[j][i])/(sum_exp_r*pow(r_matrix[j][i],2))*dr_matrix_dz[j][i];
+     d_mindist_dx[i][j]=(pow(min_dist[i],2)*exp_r[i][j])/(sum_exp_r*pow(r_matrix[i][j],2))*dr_matrix_dx[i][j];
+     d_mindist_dy[i][j]=(pow(min_dist[i],2)*exp_r[i][j])/(sum_exp_r*pow(r_matrix[i][j],2))*dr_matrix_dy[i][j];
+     d_mindist_dz[i][j]=(pow(min_dist[i],2)*exp_r[i][j])/(sum_exp_r*pow(r_matrix[i][j],2))*dr_matrix_dz[i][j];
     }
   }
   
