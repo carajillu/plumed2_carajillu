4634f64bc17988065909f5f22a92b10230792ef5
carajillu <joan.clark88@gmail.com>
2019-12-28 12:28:45 +0000

    Retested and fixed test prints.

--- src/JEDI/depth.cpp
+++ src/JEDI/depth.cpp
@@ -34,7 +34,10 @@ void Activity::compute_depth(vector<vector<int>> &neighbours, vector<S_off> prox
   cout << "Testing neighbours proximity" << endl;
   for (unsigned i=0; i<neighbours.size();i++)
   {
-   cout << "proximity point " << i << " = " << proximity.S_off_grid_vector[i] << endl;
+   for (unsigned k=0;k<neighbours[i].size();k++)
+   {
+     cout << "proximity of point " << neighbours[i][k] <<"=" << proximity[neighbours[i][k]].S_off_value << endl;
+   }
    cout << "depth point " << i << " = " << depth[i] << endl;
    for (unsigned j=0; j<d_depth_dx[i].size();j++)
    { 
