43be668cf8ab9e7d64f0d3d3bfca4a514681361a
Joan Clark-Nicolas <joan.clark88@gmail.com>
2025-03-14 12:50:45 +0000

    get benchmark from cryptobench json

--- src/GHOSTPROBE/python/get_benchmark.py
+++ src/GHOSTPROBE/python/get_benchmark.py
@@ -5,10 +5,30 @@ import subprocess
 import os
 import argparse
 import mdtraj
+import numpy as np
+import sys
+import shutil
+
+excluded_residues = [
+    # WATER
+    "WAT", "HOH", "SOL",
+
+    #HEME GROUPS
+    "HEM",  # Heme B (most common, hemoglobin, cytochromes)
+    "HEC",  # Heme C (cytochrome C, covalently bound)
+    "HEA",  # Heme A (cytochrome c oxidase)
+    "HEM3", # Iron(III) protoporphyrin IX (oxidized heme)
+    "HEO",  # Heme O (precursor to Heme A, bacterial cytochromes)
+    "DHE",  # Deuterated Heme (experimental use)
+    "HDD",  # Heme D (bacterial oxidases)
+    "HAS"  # Heme AS (bacterial variant of Heme A)
+]
+
 
 def parse():
     parser = argparse.ArgumentParser(description="Process some floats.")
     parser.add_argument('--json', type=str, default="cryptobench.json", help='cryptobench dataset (json)')
+    parser.add_argument("--r_max",type=float,default=0.6,help="maximum distance to consider that protein and ligand atoms are interacting.")
     args = parser.parse_args()
     return args
 
@@ -77,34 +97,143 @@ def pdb_get_chain(pdb_str: str, holo_chain: str, ligand_chain: str):
         chain_lst.append(line)
     return("\n".join(chain_lst))
 
+def filter_atoms_by_distance(A: mdtraj.Trajectory, B: mdtraj.Trajectory, frame: int=0, r_max: float=0.6):
+    """
+    Returns a new MDTraj object containing only the atoms in A that are within r_max distance
+    from the closest atom in B.
+    
+    Parameters:
+        A (md.Trajectory): The first MDTraj object (atoms to filter).
+        B (md.Trajectory): The second MDTraj object (reference atoms).
+        r_max (float): The maximum distance threshold.
+    
+    Returns:
+        md.Trajectory: A new MDTraj object containing only the selected atoms from A.
+    """
+    # Compute pairwise distances between all atoms in A and all atoms in B
+    xyz_A=A.xyz[frame]
+    xyz_B=B.xyz[frame]
+    distances=[]
+    # Efficient computation of pairwise distances using broadcasting
+    distances = np.linalg.norm(xyz_A[:, np.newaxis, :] - xyz_B[np.newaxis, :, :], axis=-1)
 
-def build_benchmark(df:pd.DataFrame):
+    
+    # Reshape to get the minimum distance to any atom in B for each atom in A
+    min_distances = distances.reshape(A.n_atoms, B.n_atoms).min(axis=1)
+    
+    # Select atoms in A that have at least one neighbor in B within r_max
+    selected_indices = np.where(min_distances < r_max)[0]
+    
+    # Slice the trajectory to keep only the selected atoms
+    filtered_traj = A.atom_slice(selected_indices)
+    
+    return filtered_traj
+
+def build_benchmark(df:pd.DataFrame,r_max:float=0.6):
     rootdir=os.getcwd()
+    n_success=[]
+    n_fail_noligand=[]
+    n_fail_noreceptor=[]
+    n_fail_mdtraj=[]
+    n_fail_download=[]
     for i in range(len(df.index)):
+        
         holo_pdb_id=df.holo_pdb_id[i]
-        holo_chain=df.holo_chain[i]
-        ligand_chain=df.ligand_chain[i]
-        os.makedirs(holo_pdb_id,exist_ok=True)
-        os.chdir(holo_pdb_id)
-        filtered_pdb=pdb_get_chain(pypdb.get_pdb_file(holo_pdb_id),holo_chain,ligand_chain)
+
+        #Ligand might have been in excluded gorups
+        if df.ligand[i] in excluded_residues:
+            print(f"************************* {holo_pdb_id} ligand not allowed: {df.ligand[i]} *************************")
+            n_fail_noligand.append(holo_pdb_id)
+            continue
+
+        #get pdb structure
+        pdb_str=pypdb.get_pdb_file(holo_pdb_id)
+        if pdb_str is None: #if get_pdb fails, it returns None
+           n_fail_download.append(holo_pdb_id)
+           print(f"************************* failed to download file {holo_pdb_id}.pdb  *************************")
+           if os.path.isfile(f"{holo_pdb_id}.pdb"):
+                shutil.rmtree(f"{holo_pdb_id}.pdb")
+           continue
 
         with open(f"{holo_pdb_id}.pdb","w") as f:
-            f.write(filtered_pdb)
+             f.write(pdb_str)
 
-        structure=mdtraj.load(f"{holo_pdb_id}.pdb")
+        try:
+            structure_obj=mdtraj.load(f"{holo_pdb_id}.pdb")
+        except:
+            n_fail_mdtraj.append(holo_pdb_id)
+            print(f"************************* {holo_pdb_id} cannot be loaded with mdtraj *************************")
+            continue
         
-        pocket_sel=structure.topology.select(df.holo_mdtraj_selection[i])
-        pocket_obj=structure.atom_slice(pocket_sel)
-        pocket_obj.save_gro("pocket.gro") # so that we can use make_ndx
+        #filter unwanted things
+        selection=[]
+        for atom in structure_obj.topology.atoms:
+            if atom.residue.name not in excluded_residues and atom.element!=mdtraj.element.hydrogen:
+                selection.append(atom.index)
+        structure_obj=structure_obj.atom_slice(selection)
+        
 
-        ligand_sel=structure.topology.select(df.ligand_mdtraj_selection[i])
-        print(df.ligand_mdtraj_selection[i])
-        print(ligand_sel)
-        ligand_obj=structure.atom_slice(ligand_sel)
-        ligand_obj.save_pdb("ligand.pdb")
+        #Get get ligand and receptor objects
+        rec_sel=[]
+        lig_sel=structure_obj.topology.select(df.ligand_mdtraj_selection[i])
+        for atom in structure_obj.topology.atoms:
+            if atom.index not in lig_sel:
+               rec_sel.append(atom.index)
+
         
+        ligand=structure_obj.atom_slice(lig_sel)
+        receptor=structure_obj.atom_slice(rec_sel)
+        
+        
+        #Filter atoms that are too far
+        ligand_slice=filter_atoms_by_distance(ligand,receptor,r_max=r_max)
+        receptor_slice=filter_atoms_by_distance(receptor,ligand_slice,r_max=r_max)
+        if (ligand_slice.n_atoms==0) or (receptor_slice.n_atoms==0):
+            n_fail_noreceptor.append(holo_pdb_id)
+            print(f"************************* ligand in {holo_pdb_id} seems too far from the protein to be correct *************************")
+            continue
+
+        #export structures
+        rootdir=os.getcwd()
+        os.makedirs(holo_pdb_id,exist_ok=True)
+        os.chdir(holo_pdb_id)
+        ligand_slice.save_pdb("ligand.pdb")
+        receptor_slice.save_gro("pocket.gro")
+        shutil.move(f"{rootdir}/{holo_pdb_id}.pdb",f"{rootdir}/{holo_pdb_id}/{holo_pdb_id}.pdb")
         os.chdir(rootdir)
-    
+        n_success.append(holo_pdb_id)
+        print(f"************************* successfully exported structures for {holo_pdb_id} *************************")
+
+    print(f"Number of successfully processed structres: {len(n_success)}")
+
+    print(f"Number of structures that failed due to illegal ligand: {len(n_fail_noligand)}")
+    if len(n_fail_noligand)>0:
+       print(n_fail_noligand)
+       os.makedirs("illegal_ligands",exist_ok=True)
+       for item in n_fail_noligand:
+           shutil.move(f"{rootdir}/{item}.pdb",f"{rootdir}/illegal_ligands/{item}.pdb")
+
+    print(f"Number of structures that failed due to wrong ligand (far from receoptor): {len(n_fail_noreceptor)}")
+    if len(n_fail_noreceptor)>0:
+       print(n_fail_noreceptor)
+       os.makedirs("wrong_ligands",exist_ok=True)
+       for item in n_fail_noreceptor:
+           shutil.move(f"{rootdir}/{item}.pdb",f"{rootdir}/illegal_ligands/{item}.pdb")
+
+    print(f"Number of structures that couuld not be loaded on mdtraj: {len(n_fail_mdtraj)}")
+    if len(n_fail_mdtraj)>0:
+       print(n_fail_mdtraj)
+       os.makedirs("mdtraj_error",exist_ok=True)
+       for item in n_fail_mdtraj:
+           shutil.move(f"{rootdir}/{item}.pdb",f"{rootdir}/illegal_ligands/{item}.pdb")
+
+    print(f"Number of structures that could not be downloaded: {len(n_fail_download)}")
+    if len(n_fail_download)>0:
+       print(n_fail_download)
+       os.makedirs("download_error",exist_ok=True)
+       for item in n_fail_download:
+           shutil.move(f"{rootdir}/{item}.pdb",f"{rootdir}/illegal_ligands/{item}.pdb")
+
     return
 
 
@@ -113,4 +242,5 @@ if __name__=="__main__":
     args=parse()
     df=json_to_pd(args.json)
     print(df)
-    build_benchmark(df)
+    df.to_csv("cryptobench_main.csv", index=False, sep=" ")
+    build_benchmark(df,r_max=args.r_max)
